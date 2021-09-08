f8e9d1b8fd2f8fdd9fddfff62dfed3d4d7df94df
Umang Patel <umangpatel1991.cmail.com>
2021-09-08 00:47:34 -0400

    Modified Chart editor by adding fuctionality to dynamically change Bar Values Position, Chart Title, Title Font Size, Title Color, Bar Color, Axis Label Color, Data Points Color, Bar Label Color

--- js/myScript.js
+++ js/myScript.js
@@ -13,23 +13,30 @@ function calculateHeightOfBarChartComponents(data, noOfRows) {
   };
 }
 
+var rangeSlider = function(range, value){
+  range.on('input', function(){
+    $(this).next(value).html(this.value);
+    $(".chartHead").css("font-size", this.value + "px");
+  });
+};
+
 function createChartElement(chartTitle, chartDescription, element) {
   element.css({
     "display": "flex",
+    "background-color": "transparent",
     "flex-direction": "column"
   });
 
   if(chartTitle) {
     element.append('<div class="chartHead">' + chartTitle + '</div>');
     $(".chartHead").css({
-      "background-color": "lemonchiffon",
-      "text-align": "center"
+      "text-align": "center",
+      "font-weight": "bold"
     });
   }
 
   element.append('<div class="chartBody"></div>');
   $(".chartBody").css({
-    "background-color": "lightblue",
     "flex": "1",
     "display": "flex"
   });
@@ -37,7 +44,6 @@ function createChartElement(chartTitle, chartDescription, element) {
   if(chartDescription) {
     element.append('<div class="chartFooter">' + chartDescription + '</div>');
     $(".chartFooter").css({
-      "background-color": "mistyrose",
       "text-align": "center"
     });
   }
@@ -53,9 +59,11 @@ function createChartBody(element) {
     "grid-template-rows": "[chartAreaRow-start] 1fr [chartAreaRow-end xAxisRuler-start] auto [xAxisRuler-end xAxis-start] auto [xAxis-end xAxisTitle-start] auto [xAxisTitle-end]"
   });
 
-  element.append('<div class="chartEditor">Editor</div>');
+  element.append('<div class="chartEditor"></div>');
   $(".chartEditor").css({
-    "background-color": "rgb(0,191,255)"
+    "display": "flex",
+    "flex-direction": "column",
+    "justify-content": "center"
   });
 }
 
@@ -266,10 +274,287 @@ function createChartBars(chartRawData, noOfColumns, barChartComponentsHeight) {
     $(".chartAreaBar-" + (i + 1)).css({
       "height": heightOfCurrentBar,
       "width": "50%",
-      "background-color": "rgb(253,245,230)",
-      "text-align": "center"
+      "background-color": "lightgrey",
+      "display": "flex",
+      "justify-content": "center"
     });
   }
+}
+
+function createChartEditor(element) {
+  var editorOpen = false;
+  element.append('<div class="editorToggle"><</div>');
+  $(".editorToggle").css({
+    "padding": "5px",
+    "font-size": "25px"
+  });
+  $(".editorToggle").on("click", function () {
+    if(editorOpen) {
+      $(this).html("<");
+      $(".editorBody").css("display", "none");
+    } else {
+      $(this).html(">");
+      $(".editorBody").css("display", "block");
+    }
+
+    editorOpen = !editorOpen;
+  });
+  $(".editorToggle").on("mouseover", function () {
+    $(this).css("cursor", "pointer");
+  });
+
+  element.append('<div class="editorBody"></div>');
+  $(".editorBody").css({
+    "flex": "1",
+    "display": "none"
+  });
+}
+
+function createBarValuePositionBody(element) {
+  element.css({
+    "display": "flex",
+    "flex-direction": "column",
+    "align-content": "center"
+  });
+  element.append('<div class="barValuePositionTop">Top</div>');
+  $(".barValuePositionTop").on("click", function () {
+    $(".chartAreaBar").css("align-items", "flex-start");
+  });
+
+  element.append('<div class="barValuePositionCenter">Center</div>');
+  $(".barValuePositionCenter").on("click", function () {
+    $(".chartAreaBar").css("align-items", "center");
+  });
+
+  element.append('<div class="barValuePositionBottom">Bottom</div>');
+  $(".barValuePositionBottom").on("click", function () {
+    $(".chartAreaBar").css("align-items", "flex-end");
+  });
+
+  element.children().css("border", "solid 1px lightgray");
+
+  element.children().on("mouseover", function () {
+    $(this).css("cursor", "pointer");
+  });
+}
+
+function createChartTitleBody(element) {
+  element.css({
+    "display": "flex",
+    "padding": "10px 0px"
+  });
+
+  element.append('<input type="text" id="chartTitleModifier" value="' + $('.chartHead').val() +'" />');
+
+  $('#chartTitleModifier').on('keyup', function() {
+    $('.chartHead').html($(this).val());
+  });
+
+}
+
+function createTitleFontSizeBody(element) {
+  element.css({
+    "display": "flex",
+    "padding": "10px 0px"
+  });
+
+  element.append('<div class="sliderContainer" id="titleFontSizeSliderContainer"></div>');
+
+  $("#titleFontSizeSliderContainer").append(
+    '<input type="range" min="10" max="50" value="24" step="1" class="slider" id="titleFontSizeSlider">'
+  );
+  $("#titleFontSizeSliderContainer").append(
+    '<span class="sliderValue" id="titleFontSizeSliderValue">0</span>'
+  );
+
+  var titleFontSize= $('#titleFontSizeSlider').val();
+  $('#titleFontSizeSliderValue').html(titleFontSize);
+  $(".chartHead").css("font-size", titleFontSize + "px");
+
+  rangeSlider(
+    $('#titleFontSizeSlider'),
+    $('#titleFontSizeSliderValue')
+  );
+}
+
+function createColorBody(inputType, element) {
+  var inputTypeClass;
+  var colorPalette = [
+    {"name": "ColorWhite", "value": "rgb(255, 255, 255)"},
+    {"name": "ColorGreen", "value": "rgb(30, 132, 73)"},
+    {"name": "ColorRed", "value": "rgb(176, 58, 46)"},
+    {"name": "ColorBlue", "value": "rgb(40, 116, 166)"},
+    {"name": "ColorGray", "value": "rgb(97, 106, 107)"},
+    {"name": "ColorBlack", "value": "rgb(0, 0, 0)"}
+  ];
+
+  element.css({
+    "display": "flex",
+    "flex-direction": "column",
+    "align-content": "center",
+    "height": "150px"
+  });
+
+  var inputTypeGeneralClass = inputType + "ColorPalette";
+
+  for(var currentColor of colorPalette) {
+    inputTypeClass = inputType + currentColor.name;
+    element.append('<div class="' + inputTypeClass + ' ' + inputTypeGeneralClass + '"></div>');
+    $("." + inputTypeClass).css({
+      "flex": "1",
+      "background-color": currentColor.value
+    });
+  }
+
+  if(inputType === "titleColor") {
+    $("." + inputTypeGeneralClass).on("click", function () {
+      $(".chartHead").css("color", $(this).css("background-color"));
+    });
+  }
+  else if(inputType === "barColor") {
+    $("." + inputTypeGeneralClass).on("click", function () {
+      $(".chartAreaBar").css("background-color", $(this).css("background-color"));
+    });
+  }
+  else if(inputType === "axisLabelColor") {
+    $("." + inputTypeGeneralClass).on("click", function () {
+      $(".yAxisTitleSpan").css("color", $(this).css("background-color"));
+      $(".xAxisTitleSpan").css("color", $(this).css("background-color"));
+    });
+  }
+  else if(inputType === "dataPointColor") {
+    $("." + inputTypeGeneralClass).on("click", function () {
+      $(".yAxisDataPointMark").css("background-color", $(this).css("background-color"));
+      $(".yAxisDataPointContainer").css("color", $(this).css("background-color"));
+      $(".xAxisDataPointContainer").css("color", $(this).css("background-color"));
+    });
+  }
+  else if(inputType === "barLabelColor") {
+    $("." + inputTypeGeneralClass).on("click", function () {
+      $(".chartAreaBar").css("color", $(this).css("background-color"));
+    });
+  }
+
+  element.children().css("border", "solid 1px lightgray");
+
+  element.children().on("mouseover", function () {
+    $(this).css("cursor", "pointer");
+  });
+}
+
+function createEditorBodyIndividualElement(elementDedails, parentElement) {
+  parentElement.append('<div class="editorBodyElement ' + elementDedails.class + '"></div>');
+}
+
+function createEditorBodyElementTitle(elementDedails, parentElement) {
+  parentElement.append(
+    '<span class="editorBodyElementTitle ' + elementDedails.class + '">' + elementDedails.title + '</span>'
+  );
+
+  $(".editorBodyElementTitle").css({
+    "display": "block",
+    "cursor": "pointer"
+  });
+}
+function createEditorBodyElementBody(elementDedails, parentElement) {
+  parentElement.append(
+    '<div class="editorBodyElementBody ' + elementDedails.class + '"></div>'
+  );
+
+  $(".editorBodyElementBody").css({
+    "display": "block"
+  });
+}
+
+function createEditorBodyElements(element) {
+  var editorBodyElements = [
+    {
+      "className": "barValuePositionContainer",
+      "titleClassName": "barValuePositionTitle",
+      "title": "Bar Values Position",
+      "bodyClassName": "barValuePositionBody"
+    },
+    {
+      "className": "chartTitleContainer",
+      "titleClassName": "chartTitleTitle",
+      "title": "Chart Title",
+      "bodyClassName": "chartTitleBody"
+    },
+    {
+      "className": "titleFontSizeContainer",
+      "titleClassName": "titleFontSizeTitle",
+      "title": "Title Font Size",
+      "bodyClassName": "titleFontSizeBody"
+    },
+    {
+      "className": "titleColorContainer",
+      "titleClassName": "titleColorTitle",
+      "title": "Title Color",
+      "bodyClassName": "titleColorBody"
+    },
+    {
+      "className": "barColorContainer",
+      "titleClassName": "barColorTitle",
+      "title": "Bar Color",
+      "bodyClassName": "barColorBody"
+    },
+    {
+      "className": "axisLabelColorContainer",
+      "titleClassName": "axisLabelColorTitle",
+      "title": "Axis Label Color",
+      "bodyClassName": "axisLabelColorBody"
+    },
+    {
+      "className": "dataPointColorContainer",
+      "titleClassName": "dataPointColorTitle",
+      "title": "Data Points Color",
+      "bodyClassName": "dataPointColorBody"
+    },
+    {
+      "className": "barLabelColorContainer",
+      "titleClassName": "barLabelColorTitle",
+      "title": "Bar Label Color",
+      "bodyClassName": "barLabelColorBody"
+    }
+  ];
+
+  for(var editorBodyElement of editorBodyElements) {
+    createEditorBodyIndividualElement(
+      {"class": editorBodyElement.className},
+      element
+    );
+
+    createEditorBodyElementTitle(
+      {
+        "class": editorBodyElement.titleClassName,
+        "title": editorBodyElement.title
+      },
+      $("." + editorBodyElement.className)
+    );
+
+    createEditorBodyElementBody(
+      {
+        "class": editorBodyElement.bodyClassName
+      },
+      $("." + editorBodyElement.className)
+    );
+  }
+
+  createBarValuePositionBody($(".barValuePositionBody"));
+  createChartTitleBody($(".chartTitleBody"));
+  createTitleFontSizeBody($(".titleFontSizeBody"));
+  createColorBody("titleColor", $(".titleColorBody"));
+  createColorBody("barColor", $(".barColorBody"));
+  createColorBody("axisLabelColor", $(".axisLabelColorBody"));
+  createColorBody("dataPointColor", $(".dataPointColorBody"));
+  createColorBody("barLabelColor", $(".barLabelColorBody"));
+
+  $(".editorBodyElementBody").hide();
+
+  $(".editorBodyElementTitle").on("click", function () {
+    $(".editorBodyElementBody").hide();
+    $(this).next('.editorBodyElementBody').show();
+  });
 }
 
 function drawBarChart(data, options, element) {
@@ -309,6 +594,10 @@ function drawBarChart(data, options, element) {
   );
 
   createChartBars(data.chartRawData, noOfColumns, barChartComponentsHeight);
+
+  createChartEditor($(".chartEditor"));
+
+  createEditorBodyElements($(".editorBody"));
 
   return true;
 }
