214f56cb1bb22e653a0f0b53bece5f7bef3c8e64
Umang Patel <umangpatel1991.cmail.com>
2021-10-04 12:05:06 -0400

    added chart 4

---
+++ js/chart-data4.js
@@ -0,0 +1,32 @@
+const chartData = {};
+const chartOptions = {};
+
+chartData["chartRawData"] = {
+  "Apples": {"John": 5, "Jane": 2, "Joe": 3},
+  "Oranges": {"John": 3, "Jane": 2, "Joe": 4},
+  "Pears": {"John": 4, "Jane": 3, "Joe": 4},
+  "Grapes": {"John": 7, "Jane": 2, "Joe": 2},
+  "Bananas": {"John": 2, "Jane": 1, "Joe": 5},
+  "Dragon Fruit": { "John": 5, "Jane": 2, "Joe": 3 },
+  "Pomegranate": { "John": 3, "Jane": 2, "Joe": 4 },
+  "Quararibea cordata": { "John": 4, "Jane": 3, "Joe": 4 },
+  "Yellow Passion Fruit": { "John": 7, "Jane": 2, "Joe": 2 }/* ,
+  "Gooseberries": { "John": 2, "Jane": 1, "Joe": 5 },
+  "Guava": { "John": 5, "Jane": 2, "Joe": 3 },
+  "Cantaloupe": { "John": 3, "Jane": 2, "Joe": 4 },
+  "Apricots": { "John": 4, "Jane": 3, "Joe": 4 },
+  "Evergreen Huckleberry": { "John": 7, "Jane": 2, "Joe": 2 },
+  "Lychee": { "John": 2, "Jane": 1, "Joe": 5 } */
+};
+chartData["chartTitle"] = "Fruit Conception";
+chartData["xAxisTitle"] = "Fruits";
+chartData["yAxisTitle"] = "Conception";
+chartData["yAxisRange"] = {"type": "number"};
+//chartData["yAxisRange"] = {"type": "number", "prefix": "", "postfix": ""};
+//chartData["yAxisRange"] = {"min": 0, "max": 12, "type": "number", "prefix": "", "postfix": ""};
+chartData["yAxisRange"] = {"min": 0, "max": 12, "gap": 3, "type": "number", "prefix": "", "postfix": ""};
+
+chartOptions["chartHeight"] = 600;  //height in px
+chartOptions["chartWidth"] = 600;   //width in px
+
+export {chartData as chartData4, chartOptions as chartOptions4};
--- js/myScript.js
+++ js/myScript.js
@@ -2,6 +2,7 @@ import drawBarChart from './my-bar-chart.js';
 import {chartData1, chartOptions1} from './chart-data1.js';
 import {chartData2, chartOptions2} from './chart-data2.js';
 import {chartData3, chartOptions3} from './chart-data3.js';
+import { chartData4, chartOptions4 } from './chart-data4.js';
 
 $.when( $.ready ).then(function() {
   const chartElement1 = $("#chart-container1");
@@ -12,4 +13,7 @@ $.when( $.ready ).then(function() {
 
   const chartElement3 = $("#chart-container3");
   drawBarChart(chartData3, chartOptions3, chartElement3);
+
+  const chartElement4 = $("#chart-container4");
+  drawBarChart(chartData4, chartOptions4, chartElement4);
 });
