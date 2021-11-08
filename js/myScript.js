6634f44cd043b1d5aeb632de281d37d1dbf04a47
Umang Patel <umangpatel1991@gmail.com>
2021-11-07 19:36:59 -0500

    modify number of charts to shows up

--- js/myScript.js
+++ js/myScript.js
@@ -2,9 +2,9 @@ import drawBarChart from './my-bar-chart.js';
 import {chartData1, chartOptions1} from './chart-data1.js';
 import {chartData2, chartOptions2} from './chart-data2.js';
 import {chartData3, chartOptions3} from './chart-data3.js';
-import { chartData4, chartOptions4 } from './chart-data4.js';
+/* import { chartData4, chartOptions4 } from './chart-data4.js';
 import { chartData5, chartOptions5 } from './chart-data5.js';
-import { chartData6, chartOptions6 } from './chart-data6.js';
+import { chartData6, chartOptions6 } from './chart-data6.js'; */
 
 $.when( $.ready ).then(function() {
   const chartElement1 = $("#chart-container1");
@@ -16,12 +16,12 @@ $.when( $.ready ).then(function() {
   const chartElement3 = $("#chart-container3");
   drawBarChart(chartData3, chartOptions3, chartElement3);
 
-  const chartElement4 = $("#chart-container4");
+  /* const chartElement4 = $("#chart-container4");
   drawBarChart(chartData4, chartOptions4, chartElement4);
 
   const chartElement5 = $("#chart-container5");
   drawBarChart(chartData5, chartOptions5, chartElement5);
 
   const chartElement6 = $("#chart-container6");
-  drawBarChart(chartData6, chartOptions6, chartElement6);
+  drawBarChart(chartData6, chartOptions6, chartElement6); */
 });
